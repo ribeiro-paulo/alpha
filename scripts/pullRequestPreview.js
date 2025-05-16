8173166b7a8d1c8cb8840afe144f85fd938b0114
Paulo Ribeiro <ribeiro.paulotm@gmail.com>
2025-05-15 23:30:46 -0400

    feat: github PR deploy auto comment

--- scripts/pullRequestPreview.js
+++ scripts/pullRequestPreview.js
@@ -1,5 +1,5 @@
 import { execSync } from "child_process";
-// import fetch from "node-fetch";
+import { Octokit } from "octokit";
 
 console.log("[DEPLOY_PREVIEW]: START");
 const command = "npm run deploy:staging";
@@ -8,44 +8,45 @@ const outputLines = output.split("\n");
 const DEPLOY_URL = outputLines[outputLines.length - 1];
 console.log("[DEPLOY_PREVIEW]: END");
 
-console.log(`You can see the deploy preview on: ${DEPLOY_URL}`);
-
 // ===================================
 // ===================================
 
 console.log("[GITHUB_COMMENT]: START");
-const { GITHUB_TOKEN, GITHUB_REPOSITORY, GITHUB_PR_NUMBER } = process.env;
+const {
+  GITHUB_TOKEN,
+  GITHUB_REPOSITORY_OWNER,
+  GITHUB_PR_NUMBER,
+  GITHUB_REPOSITORY,
+} = process.env;
 const GH_COMMENT = `
-- Deploy URL: [${DEPLOY_URL}](${DEPLOY_URL})
+- Deploy URL TEST: [${DEPLOY_URL}](${DEPLOY_URL})
 `;
 
 const defaultHeaders = {};
+defaultHeaders["User-Agent"] = `Awesome-Octocat-App`;
 defaultHeaders["authorization"] = `token ${GITHUB_TOKEN}`;
 defaultHeaders["accept"] =
   "application/vnd.github.v3+json; application/vnd.github.antiope-preview+json";
 defaultHeaders["content-type"] = "application/json";
-
-console.log("GITHUB_REPOSITORY", GITHUB_REPOSITORY);
+console.log("GITHUB_REPOSITORY_OWNER", GITHUB_REPOSITORY_OWNER);
 console.log("GITHUB_PR_NUMBER", GITHUB_PR_NUMBER);
+console.log("defaultHeaders", defaultHeaders);
+console.log("process.env", process.env);
 
-fetch(
-  `https://api.github.com/repos/${GITHUB_REPOSITORY}/issues/${GITHUB_PR_NUMBER}/comments`,
+// new --------------
+const octokit = new Octokit({
+  auth: GITHUB_TOKEN,
+});
+
+await octokit.request(
+  "POST /repos/{owner}/{repo}/issues/{issue_number}/comments",
   {
-    headers: defaultHeaders,
-    method: "POST",
-    body: JSON.stringify({
-      body: GH_COMMENT,
-    }),
+    owner: GITHUB_REPOSITORY_OWNER,
+    repo: GITHUB_REPOSITORY.split("/")[1],
+    issue_number: GITHUB_PR_NUMBER,
+    body: GH_COMMENT,
+    headers: {
+      "X-GitHub-Api-Version": "2022-11-28",
+    },
   },
-)
-  .then((response) => {
-    if (response.ok) return response.json();
-    throw new Error(response.statusText);
-  })
-  .catch((err) => {
-    console.log("[COMMENT_ON_GITHUB: ERROR]");
-    throw new Error(err);
-  })
-  .finally(() => {
-    console.log("[COMMENT_ON_GITHUB: END]");
-  });
+);
